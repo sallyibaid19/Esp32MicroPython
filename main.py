696e4b5c770a5cf05af20c4837b91476158d6408
Sally Ibaid <56202060+sallyibaid19@users.noreply.github.com>
2023-10-01 08:35:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import machine
+import time
+led = machine.Pin(2, machine.Pin.OUT)
+sw = machine.Pin(0, machine.Pin.IN)
+
+def blink_led_ntimes(num, t_on, t_off, msg):
+    counter = 0
+    while(counter < num):
+        led.on()
+        time.sleep(t_on)
+        led.off()
+        time.sleep(t_off)
+        counter += 1
+    print (msg)
+     
+while True:
+    if(sw.value() == 0):
+        blink_led_ntimes(7, 0.25, 0.50, 'Done!')
+                
+ 
